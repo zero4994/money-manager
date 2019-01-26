0e4c8761c498a73c43d3f234203842753ea7c311
zero4994 <cavc94@gmail.com>
2019-01-26 15:07:06 +0900

    Added simple configuration, base dependencies

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    database: {
+        client: "pg",
+        connection: {
+            host: "127.0.0.1",
+            database: "m_manager",
+        },
+        port: 5432
+    },
+
+    express: {
+        port: 3000,
+    }
+}
