ef0ac4bd5c017fe4b0a4f942575cfd0a6d1b612d
zero4994 <cavc94@gmail.com>
2019-01-27 00:17:03 +0900

    Added seed files for transactions

--- services/db/seeds/seed_test_data.js
+++ services/db/seeds/seed_test_data.js
@@ -1,14 +1,22 @@
 const users = require("./users.json");
 const accounts = require("./accounts.json");
+const transactions = require("./transactions.json");
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex
-    .raw("TRUNCATE TABLE users, accounts, transactions CASCADE;")
+    .raw(`TRUNCATE TABLE transactions RESTART IDENTITY CASCADE;
+          TRUNCATE TABLE accounts CASCADE;
+          ALTER SEQUENCE seq_acc_num RESTART WITH 10000000;
+          TRUNCATE TABLE users RESTART IDENTITY CASCADE;`)
     .then(() => {
       return knex("users").insert(users);
     })
     .then(() => {
       return knex("accounts").insert(accounts);
+    })
+    .then(() => {
+      return knex("transactions").insert(transactions);
     });
 };
+ 
